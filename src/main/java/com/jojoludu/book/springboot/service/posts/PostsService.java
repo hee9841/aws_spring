de91ade491451e8cad3c0e989e6499bf32489d7e
hee9841 <lee98417716@gmail.com>
2024-05-28 15:02:44 +0900

    feat: 게시글 수정, 조회
    - 게시글 수정, 조회 controller, service 추가
    - Resposne Dto, Update Requests dto
    추가

--- src/main/java/com/jojoludu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoludu/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.jojoludu.book.springboot.service.posts;
 
+import com.jojoludu.book.springboot.domain.posts.Posts;
 import com.jojoludu.book.springboot.domain.posts.PostsRepository;
+import com.jojoludu.book.springboot.web.dto.PostResponseDto;
 import com.jojoludu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoludu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        return new PostResponseDto(posts);
     }
 
 
