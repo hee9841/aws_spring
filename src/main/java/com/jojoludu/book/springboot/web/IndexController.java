9113d1f0d28c1d68d6b71d3ff4eb72fd7646fd0f
hee9841 <lee98417716@gmail.com>
2024-05-30 23:20:25 +0900

    feat: google 로그인 기능 구현하기

--- src/main/java/com/jojoludu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoludu/book/springboot/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.jojoludu.book.springboot.web;
 
+import com.jojoludu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoludu.book.springboot.service.posts.PostsService;
 import com.jojoludu.book.springboot.web.dto.PostResponseDto;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        /**
+         * 1. CustomOAuth2UserService에서 httpSession.setAttribute("user", new SessionUser(user)) 부분
+         *  로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+         *  로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수있음
+         * **/
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+
+        /**
+         * 2.
+         * 세션에 저장된 값이 있을 때만 모델에 등록
+         *
+         * */
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
