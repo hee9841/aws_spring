d8140fd3204c6d4ca4b4dbbf915ecfb203215d86
hee9841 <lee98417716@gmail.com>
2024-09-20 01:20:10 +0900

    chore: 무중단 배포를 위한 스크립트 파일 추가, appspec.yml 재구성

---
+++ scripts/profile.sh
@@ -0,0 +1,34 @@
+#!/user/bin/env bash
+# 쉬고있는 프로파일 찾기: real1, real2
+
+function find_idle_profile() {
+    RESPONSE_CODE=$(curl - s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+    if [ ${RESPONSE_CODE} -ge 400 ]
+    then
+      CURRENT_PROFILE=real2
+    else
+      CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+
+#쉬고 있는 profile 찾기
+function find_idle_port() {
+    IDLE_PROFILE=${find_idle_profile}
+
+    if [ ${IDLE_PROFILE} == null ]
+    then
+      ehco "8081"
+    else
+      ehco "8082"
+    fi
+}
